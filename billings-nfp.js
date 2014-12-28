f74847ee4a676193dbde10dabf013a518ee8588d
Tobias Berg <tobias.hult@gmail.com>
2014-12-28 09:16:03 +0100

    Updating works (hack) and route to calendar

--- billings-nfp.js
+++ billings-nfp.js
@@ -1,32 +1,5 @@
 Billings = new Mongo.Collection('billings');
 
-Billings.allow({
-  insert: function (userId, doc) {
-    // the user must be logged in, and the document must be owned by the user
-    return true;
-  },
-  update: function (userId, doc, fields, modifier) {
-    // can only change your own documents
-    return true;
-  },
-  remove: function (userId, doc) {
-    // can only remove your own documents
-    return true;
-  },
-  
-});
-
-Billings.deny({
-  update: function (userId, docs, fields, modifier) {
-    // can't change owners
-    return false;
-  },
-  remove: function (userId, doc) {
-    // can't remove locked documents
-    return false;
-  },
-});
-
 if (Meteor.isClient) {
 	// counter starts at 0
 	Session.setDefault("counter", 0);
@@ -60,25 +33,39 @@ if (Meteor.isClient) {
 		'submit .choice-info': function(event) {
 			event.preventDefault();
 			var choice = $(event.target).find('input:hidden[name=choice]').val();
-			console.log(choice);
 			var hadSex = $(event.target).find('input:radio[name=haveYouHadSex]:checked').val();
-			console.log(hadSex);
 			var comment = $(event.target).find('textarea').val();
-			console.log(comment);
+			var existingEntry = Billings.findOne({id: moment().format('YYYYMD')});
 
-			Billings.upsert({
-				id: moment().format('YYYYMD')
-			},
-			{
+			var newEntry = {
 				id: moment().format('YYYYMD'),
 				createdAt: new Date(),
 				choice: choice,
 				hadSex: hadSex,
 				comment: comment
-			}
-			);
+			};
 
-			//Router.go('/calendar');
+
+			if (existingEntry === undefined) {
+				Billings.insert(newEntry);
+			}
+			else {
+				Billings.update(
+						{_id: existingEntry._id},
+				{
+					$set:
+							{
+								id: newEntry.id,
+								createdAt: newEntry.createdAt,
+								choice: newEntry.choice,
+								hadSex: newEntry.hadSex,
+								comment: newEntry.comment
+							}
+				}
+				);
+			}
+
+			Router.go('/calendar');
 			return false;
 		}
 	});
